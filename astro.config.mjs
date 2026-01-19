31a880a85c7566e1b6c9f17a267f127387ae93c9
unknown <power6120@namver.com>
2026-01-19 22:25:45 +0900

    애드센스 재수정, 논문 인용 및 외부 링크 모듈 추가

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import rehypeExternalLinks from 'rehype-external-links'
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://blog.dimad.kr',
 	integrations: [mdx(), sitemap()],
+	markdown: {
+		rehypePlugins: [
+			[rehypeExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }]
+		]
+	}
 });
