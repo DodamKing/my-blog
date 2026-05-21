e1325c5f0d026a63d051ca7a18ba1776331bc1ec
dodamking <power6120@naver.com>
2026-05-21 21:45:08 +0900

    페이지네이션 2페이지+ noindex,follow + sitemap에서 제외 + self-canonical로 환원

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,19 @@ import sitemapLastmod from './integrations/sitemap-lastmod';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://blog.dimad.kr',
-	integrations: [react(), mdx(), sitemap(), sitemapLastmod()],
+	integrations: [
+		react(),
+		mdx(),
+		sitemap({
+			filter: (page) => {
+				const path = new URL(page).pathname;
+				if (/^\/blog\/\d+\/?$/.test(path)) return false;
+				if (/^\/blog\/[a-z-]+\/\d+\/?$/.test(path)) return false;
+				return true;
+			},
+		}),
+		sitemapLastmod(),
+	],
 	markdown: {
 		rehypePlugins: [
 			[rehypeExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }]
